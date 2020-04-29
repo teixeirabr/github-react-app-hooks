80aefbe5aff7894fea2e60749cbac4ca0d54cc2a
andre <andrelaprov@gmail.com>
2020-04-29 15:33:25 -0400

    dry code

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
-
-//stateful component
-// class App extends React.Component {
-//   this.state = {
-//     user: ''
-//   }
-
-// handleChange = () =>{
-//   this.setState({
-//     [e.target.name]=e.target.value
-//    })
-// }
-
-//   render() {
-//     return (
-
-//     )
-//   }
-// }
 
 function App() {
   const [userName, setUserName] = useState("");
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
 
-  // this.state = {
-  //   userName: ''
-  // } = useState()
-  // setUserName('andre');
-  // this.setState({ userName: 'andre' })
-
-  //setUserName
-  // this.setState({ userName: '' })
-
-  //construir uma funcao que vá no github e busque por username
   function handleGithubUser() {
-    //inicia com loading true
     setLoading(true);
-    //configura o axios e define uma base_url
+
     const api = axios.create({
       baseURL: "https://api.github.com/users/",
     });
-
-    //chamada get buscando username do usuário
     api.get(userName).then((response) => {
-      //pega a resposta e define na variavel "user"
       setUser(response.data);
-      //ao finalizar muda loading pra false
       setLoading(false);
     });
   }
+
+  // function handleGithubUser() {
+  //   setLoading(true);
+
+  //   axios.get("https://api.github.com/users/").then((response) => {
+  //     setUser(response.data);
+  //     setLoading(false);
+  //   });
+  // }
 
   // || or && and
 
